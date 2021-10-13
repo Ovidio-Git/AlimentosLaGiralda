85daf2037e0d98728d8f0b8e5d53f2d68db69047
wilbhert <cblackman3@gmail.com>
2021-10-13 09:37:04 -0500

    Archivos e Integracion Home_Login

--- forms.py
+++ forms.py
@@ -4,6 +4,13 @@ from wtforms import PasswordField, TextField, SubmitField
 from wtforms.validators import InputRequired     #validores
 
 # crea una clase que extiende de Flaskform y se llama Login()
+class LogIn(FlaskForm):     
+    
+    usr = TextField('Usuario', validators=[InputRequired(message='El campo usuario es requerido')])
+    pwd = PasswordField('Contraseña', validators=[InputRequired(message='El campo usuario es requerido')])
+    btn = SubmitField('Ingresar')
+
+
 class Search(FlaskForm):     # 
     
     name = TextField('name')
